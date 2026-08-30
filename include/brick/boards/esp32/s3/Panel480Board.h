2016a16fcf6090b5a5ba8acc0a4c57e07a94da1c
Artur Zarzecki <artur.zarzecki@techsterowniki.pl>
2026-08-30 14:04:01 +0200

    avoid redundant panel clock override

--- include/brick/boards/esp32/s3/Panel480Board.h
+++ include/brick/boards/esp32/s3/Panel480Board.h
@@ -47,7 +47,6 @@ public:
     static St7701sRgbPanelConfig double_buffered_config()
     {
         auto config = profiles::st7701s_480x480();
-        config.pixel_clock_hz = 12'000'000;
         config.frame_buffer_count = 2;
         return config;
     }
